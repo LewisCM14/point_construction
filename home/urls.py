02cfb8393d31204a4cb2d0ebb980b38260bc2f3b
LewisCM14 <Lewiscmartin@live.co.uk>
2022-10-09 12:47:35 +0100

    setup homepage

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+""" The home app URLS """
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
--- point_construction/settings.py
+++ point_construction/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Project Apps
+    'home',
 ]
 
 MIDDLEWARE = [
