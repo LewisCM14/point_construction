e3fab2a8099fc1780704a0802f2b729254fc361b
LewisCM14 <Lewiscmartin@live.co.uk>
2022-10-18 23:06:53 +0100

    impliment CompanyDetails CMS feature

--- home/company_details_ctx_processor.py
+++ home/company_details_ctx_processor.py
@@ -4,16 +4,31 @@ def get_company_details_context(request):
     """
     CompanyDetails model allows only one object.
     Collect this object and return details as context for use in templates
-    """
-    company_details = CompanyDetails.objects.get(id=1)
 
-    context = {
-        'company_email': company_details.email,
-        'company_phone_number': company_details.phone_number,
-        'company_address1': company_details.street_address1,
-        'company_address2': company_details.street_address2,
-        'company_town_or_city': company_details.town_or_city,
-        'company_postcode': company_details.postcode,
-    }
+    Defaults to initial details if no entry in database exists
+    """
+    company_details = CompanyDetails.objects.all()
+
+    if company_details.exists():
+        company_details = CompanyDetails.objects.get()
+
+        context = {
+            'company_email': company_details.email,
+            'company_phone_number': company_details.phone_number,
+            'company_address1': company_details.street_address1,
+            'company_address2': company_details.street_address2,
+            'company_town_or_city': company_details.town_or_city,
+            'company_postcode': company_details.postcode,
+        }
+
+    else:
+        context = {
+        'company_email': 'sales@point.co.uk',
+        'company_phone_number': '0115 971 8909',
+        'company_address1': 'Point Construction',
+        'company_address2': '15 The Point,',
+        'company_town_or_city': 'Nottingham',
+        'company_postcode': 'NG1 1LL',
+        }
 
     return context
